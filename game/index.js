de9c9835236674d6ba56b1ea0de5687e75231f35
Paweł Marczewski <pwmarcz@gmail.com>
2019-04-14 14:04:51 +0200

    Add movement

--- game/index.js
+++ game/index.js
@@ -6,10 +6,12 @@ const mapH = map[0].length;
 let mapSprites = [];
 
 let app = new PIXI.Application({
-  width: mapW * TILE_SIZE, 
+  width: mapW * TILE_SIZE,
   height: mapH * TILE_SIZE,
 });
 document.body.appendChild(app.view);
+
+let keys = {};
 
 PIXI.loader
 .add('tileset.auto.png')
@@ -28,12 +30,55 @@ function setup() {
       mapSprites[y][x] = sprite;
     }
   }
-  for (const m in mobiles) {    
+  for (const m in mobiles) {
     const sprite = new PIXI.Sprite(tileTextures[mobiles[m].tile]);
-    sprite.x = mobiles[m].x * TILE_SIZE;
-    sprite.y = mobiles[m].y * TILE_SIZE;
+    mobiles[m].sprite = sprite;
     app.stage.addChild(sprite);
-    mapSprites[mobiles[m].y][mobiles[m].x].alpha = 0;
+    moveMobile(mobiles[m], mobiles[m].x, mobiles[m].y);
   }
   app.renderer.render(app.stage);
+  app.ticker.add(gameLoop);
+
+  keys.up = keyboard('ArrowUp');
+  keys.down = keyboard('ArrowDown');
+  keys.left = keyboard('ArrowLeft');
+  keys.right = keyboard('ArrowRight');
+}
+
+let frameNum = 0;
+const FRAMES_PER_TURN = 15;
+
+function gameLoop(delta) {
+  frameNum += delta;
+  while (frameNum > FRAMES_PER_TURN) {
+    frameNum -= FRAMES_PER_TURN;
+    turn();
+  }
+}
+
+function turn() {
+  const mob = mobiles.player;
+  if (keys.up.isDown) {
+    moveMobile(mob, mob.x, mob.y - 1);
+  } else if (keys.down.isDown) {
+    moveMobile(mob, mob.x, mob.y + 1);
+  } else if (keys.left.isDown) {
+    moveMobile(mob, mob.x - 1, mob.y);
+  } else if (keys.right.isDown) {
+    moveMobile(mob, mob.x + 1, mob.y);
+  }
+}
+
+function moveMobile(mob, x, y) {
+  const newTile = map[y][x];
+  if (['FLOOR', 'GRASS', 'GRASS_TALL', 'DOOR_OPEN'].indexOf(newTile) === -1) {
+    return;
+  }
+
+  mapSprites[mob.y][mob.x].alpha = 1;
+  mob.x = x;
+  mob.y = y;
+  mob.sprite.x = mob.x * TILE_SIZE;
+  mob.sprite.y = mob.y * TILE_SIZE;
+  mapSprites[mob.y][mob.x].alpha = 0;
 }
