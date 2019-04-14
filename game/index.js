a6c831cea1c89fa23e38b1f246e6552d24d054d3
Paweł Marczewski <pwmarcz@gmail.com>
2019-04-14 13:26:34 +0200

    Draw player

--- game/index.js
+++ game/index.js
@@ -9,6 +9,16 @@ const map = [
 const mapW = map.length;
 const mapH = map[0].length;
 
+let mapSprites = [];
+
+let mobiles = {
+  player: {
+    x: 2, y: 2,
+    tile: 'HUMAN',
+    sprite: null,
+  }
+};
+
 let app = new PIXI.Application({
   width: 320, 
   height: 320,
@@ -22,13 +32,22 @@ PIXI.loader
 function setup() {
   prepareTextures();
   for (let y = 0; y < mapW; y++) {
+    mapSprites[y] = [];
     for (let x = 0; x < mapH; x++) {
       const tile = map[y][x];
       const sprite = new PIXI.Sprite(tileTextures[tile]);
       sprite.x = x * 32;
       sprite.y = y * 32;
       app.stage.addChild(sprite);
+      mapSprites[y][x] = sprite;
     }
+  }
+  for (const m in mobiles) {    
+    const sprite = new PIXI.Sprite(tileTextures[mobiles[m].tile]);
+    sprite.x = mobiles[m].x * 32;
+    sprite.y = mobiles[m].y * 32;
+    app.stage.addChild(sprite);
+    mapSprites[mobiles[m].y][mobiles[m].x].alpha = 0;
   }
   app.renderer.render(app.stage);
 }
