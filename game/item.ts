f9cebf97d405ad5565869f8dd6b52c83e9b4016a
Paweł Marczewski <pwmarcz@gmail.com>
2019-04-27 10:48:38 +0200

    Reindent

--- game/item.ts
+++ game/item.ts
@@ -2,25 +2,25 @@ import { Tile } from "./tiles";
 import { Pos } from "./types";
 
 export enum ItemType {
-    GOLD = 'GOLD',
-    SWORD = 'SWORD',
-    SHIELD = 'SHIELD',
-    KEY = 'KEY',
+  GOLD = 'GOLD',
+  SWORD = 'SWORD',
+  SHIELD = 'SHIELD',
+  KEY = 'KEY',
 }
 
 export class Item {
-    readonly id: string;
-    readonly type: ItemType;
-    pos: Pos | null;
-    mobId: string | null = null;
+  readonly id: string;
+  readonly type: ItemType;
+  pos: Pos | null;
+  mobId: string | null = null;
 
-    constructor(id: string, type: ItemType, pos: Pos | null) {
-        this.id = id;
-        this.type = type;
-        this.pos = pos;
-    }
+  constructor(id: string, type: ItemType, pos: Pos | null) {
+    this.id = id;
+    this.type = type;
+    this.pos = pos;
+  }
 
-    tile(): Tile {
-        return this.type as string;
-    }
+  tile(): Tile {
+    return this.type as string;
+  }
 }
--- game/mob.ts
+++ game/mob.ts
@@ -2,36 +2,36 @@ import { Tile } from "./tiles";
 import { Pos, Action } from "./types";
 
 export enum MobType {
-    GOBLIN = 'GOBLIN',
-    HUMAN = 'HUMAN',
+  GOBLIN = 'GOBLIN',
+  HUMAN = 'HUMAN',
 }
 
 interface MobMeta {
-    movementTime: number;
+  movementTime: number;
 };
 
 const MOB_META: Record<MobType, MobMeta> = {
-    [MobType.GOBLIN]: { movementTime: 20 },
-    [MobType.HUMAN]: { movementTime: 10 },
+  [MobType.GOBLIN]: { movementTime: 20 },
+  [MobType.HUMAN]: { movementTime: 10 },
 };
 
 export class Mob {
-    readonly id: string;
-    readonly type: MobType;
-    pos: Pos;
-    action: Action | null = null;
+  readonly id: string;
+  readonly type: MobType;
+  pos: Pos;
+  action: Action | null = null;
 
-    constructor(id: string, type: MobType, pos: Pos) {
-        this.id = id;
-        this.type = type;
-        this.pos = pos;
-    }
+  constructor(id: string, type: MobType, pos: Pos) {
+    this.id = id;
+    this.type = type;
+    this.pos = pos;
+  }
 
-    tile(): Tile {
-        return this.type as string;
-    }
+  tile(): Tile {
+    return this.type as string;
+  }
 
-    movementTime(): number {
-        return MOB_META[this.type].movementTime;
-    }
+  movementTime(): number {
+    return MOB_META[this.type].movementTime;
+  }
 }
